314dc71375c91970f1117061f6756924e28e8ceb
Huluette <leana.beuvant@gmail.com>
2023-10-03 16:06:31 +0200

    code principal non fonctionnel

--- main.js
+++ main.js
@@ -4,20 +4,32 @@
 
 let url = "https://reqres.in/api/users?per_page=12";
 
-// Variable de la div id root dans le html
+// Création de la fonction qui permet de récupérer les éléments de l'api
 
-const root = document.getElementById("root");
-
-// Création de la nouvelle fonction qui permet d'afficher les éléments de l'api
-
-function fecthApi() {
-
+function fetchApi() {
     fetch(url)
+        .then(response => response.json())
+        .then(data => displayUsers(data.data))
+        .catch(console.error)
+}
 
-        .then(response => response.json)
+// Création de la fonction qui permet d'afficher les éléments de l'api
 
-        .then(data => displayUsersApi(tabApi.data))
-
-        .catch(console.error="Vous n'avez pas pu récupérer les données")
+function displayUsers(users) {
+    
+    // Variable de la div id root dans le html
+    const app = document.getElementById("root");
+    
+    users.forEach(user => {
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+        <h2>${user.first_name} ${user.last_name}</h2>
+        <p>Email : ${user.email}</p>
+        <img src="${user.avatar}" alt="${user.first_name}${user.last_name}" width="100">`;        
+        app.appendChild(userDiv);
+    });
 
 }
+
+
+
