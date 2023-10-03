5ab59d800c61bfd5a4796e04fe4cf088f23e37b7
Huluette <leana.beuvant@gmail.com>
2023-10-03 13:38:23 +0200

    sauvegarde test var root

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 // Utilisation de la méthode fecth //
 
-fetch("")
+let url = "https://reqres.in/api/users?per_page=12";
+let root = document.getElementById('root')
+let rootJSON;
 
-    .then(function (response) {
-        return response.();
+fetch(url)
+
+    .then(response => response.json())
+
+    .then(data => {
+
+        root = url() {`
+        <div id="card">
+            <img src="${root.avatar}" alt="image de l'api">
+            <p>${root.first_name}</p>
+            <p>${root.last_name}</p>
+        </div>
+        `
+        }
     })
 
-    .then(function () {
-       
-    });
+    .catch(function(error) {
+        console.error('Erreur de chargement de URL:', error);
+    })
